14104d568d20732826cf2d291b84368d5cdb11a7
Davidev <deehzi@pm.me>
2022-02-16 21:51:32 -0300

    Added a screen that appears whenever the task list becomes empty

--- app/src/main/java/com/davidev/chronos/ui/MainActivity.kt
+++ app/src/main/java/com/davidev/chronos/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.davidev.chronos.ui
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.davidev.chronos.databinding.ActivityMainBinding
 import com.davidev.chronos.datasource.TaskDataSource
@@ -48,6 +49,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateList() {
         val list = TaskDataSource.getList()
+
+            binding.includeEmpty.emptyState.visibility = if (list.isEmpty())  View.VISIBLE
+        else  View.GONE
+
+
         adapter.submitList(list)
     }
 
